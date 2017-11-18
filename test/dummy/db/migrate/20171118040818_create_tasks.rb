f122978a32517a9be866b3c10a52dede0f026e21
Martin-Alexander <martingianna@gmail.com>
2017-11-17 23:10:49 -0500

    ran migrations

---
+++ test/dummy/db/migrate/20171118040818_create_tasks.rb
@@ -0,0 +1,11 @@
+class CreateTasks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.date :start
+      t.date :finish
+      t.integer :progress
+      t.string :dependencies
+    end
+  end
+end
