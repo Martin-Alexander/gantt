6519b2ff8980478740ec45c8760aef0d10b96b99
Martin-Alexander <martingianna@gmail.com>
2017-11-17 23:39:54 -0500

    can display gantt chart (no server connection)

--- assets/javascripts/gantt.js
+++ assets/javascripts/gantt.js
@@ -2,6 +2,20 @@
 //= require moment
 //= require frappe-gnatt
 
-var RailsGantt = function(taskJSON) {
-	console.log(taskJSON);
+var RailsGantt = function(element, taskJSON) {
+	this.tasks = formattedTasks(taskJSON);
+	this.create(element);
+}
+
+var formattedTasks = function(tasks) {
+	tasks.forEach(function(task) {
+		task.end = task.finish;
+		task.id = task.id.toString();
+		delete task.finish;
+	});
+	return tasks;
+}
+
+RailsGantt.prototype.create = function(element) {
+	this.gantt = new Gantt(element, this.tasks);
 }
