b74934632a7f9e8649643533f93c4dff285657ab
Martin-Alexander <martingianna@gmail.com>
2017-11-19 15:40:47 -0500

    refactoring add add new functionality relating to managing dependencies : :

--- test/dummy/app/controllers/application_controller.rb
+++ test/dummy/app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def main
+	def main
+		puts Task.first.is_dependency_for
     @tasks = Task.all.as_gantt_tasks
   end
   
